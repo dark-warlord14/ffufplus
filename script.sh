744103f9f3546e1cd71107babe1bfee1e9233f4e
root <root@DESKTOP-K2VP55T.localdomain>
2020-04-12 20:53:23 +0530

    Update

--- script.sh
+++ script.sh
@@ -1,46 +1,56 @@
 #!/bin/bash
 
-echo "ffuf on steroids"
-PS3="Select one of the options from above : "
-choices=("Parameter Discovery" "VHOST Discovery" "Parse waybackurls" "Beast Mode")
+printf "\nffuf on Steroids\n\n"
+PS3="
+Select one of the options from above : "
+choices=("Directory BruteForcing" "Parameter Discovery" "VHOST Discovery" "Parse waybackurls" "Beast Mode")
 select choice in "${choices[@]}"; do
         case $choice in
+		"Directory BruteForcing")
+			printf "\nDirectory Bruteforcing"
+			printf "\n\nEnter the url for Directory Bruteforcing: "
+			read input1
+			ffuf -mc all -c -H "X-Forwarded-For: 127.0.0.1" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0" -u "$input1/FUZZ" -w wordlist/dicc.txt -D -e js,php,bak,txt,asp,aspx,jsp,html,zip,jar,sql,json,old,gz,shtml,log,swp,yaml,yml,config,save,rsa,ppk -ac -o result_dir.tmp
+			cat result_dir.tmp | jq '[.results[]|{status: .status, length: .length, url: .url}]' | grep -oP "status\":\s(\d{3})|length\":\s(\d{1,7})|url\":\s\"(http[s]?:\/\/.*?)\"" | paste -d' ' - - - | awk '{print $2" "$4" "$6}' | sed 's/\"//g' > result_dir.txt
+			printf "\nDone. Result is stored in result_dir.txt\n"
+			break
+			;;
                 "Parameter Discovery")
-                        echo "Parameter Discovery"
-                        echo "Enter URL to discover parameter for"
+                        printf "\nParameter Discovery"
+                        printf "\n\nEnter URL to discover parameter for: "
                         read input1
-                        ffuf -c -H "X-Forwarded-For: 127.0.0.1" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0" -u "$input1?FUZZ=abcd" -w wordlist/param.txt -ac -s | tee result_param.txt
-			echo "Done. Result is stored in result_param.txt"
+                        ffuf -mc all -c -H "X-Forwarded-For: 127.0.0.1" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0" -u "$input1?FUZZ=abcd" -w wordlist/param.txt -ac | tee result_param.txt
+			printf "\nDone. Result is stored in result_param.txt\n"
                         break
                         ;;
                 "VHOST Discovery")
-                        echo "VHOST Discovery"
-                        echo "Enter domain for Virtual Host Discovery"
+                        printf "\nVHOST Discovery"
+                        printf "\n\nEnter domain for Virtual Host Discovery: "
                         read input1
                         domain=$(echo "$input1" | unfurl -u domain)
-                        ffuf -c -H "X-Forwarded-For: 127.0.0.1" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0" -u "$input1" -H "Host: FUZZ.$domain" -w wordlist/vhost.txt -ac -s | tee result_vhost.txt
-			echo "Done. Result is stored in result_vhost.txt"
+                        ffuf -mc all -c -u "$input1" -H "Host: FUZZ.$domain" -w wordlist/vhost.txt -ac -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0" -o result_vhost.txt
+			printf "\nDone. Result is stored in result_vhost.txt\n"
                         break
                         ;;
                 "Parse waybackurls")
-                        echo "Gathering waybackurls, otxUrls also commoncrawl data"
-                        echo "Enter the domain"
+                        printf "\nGathering waybackurls, otxUrls also commoncrawl data"
+                        printf "\n\nEnter the domain: "
                         read input1
                         domain=$(echo "$input1" | unfurl -u domain)
                         gau $domain > gau.tmp
-                        ffuf -c -H "X-Forwarded-For: 127.0.0.1" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0" -u FUZZ -w gau.tmp -o result_gau.tmp
+                        ffuf -mc all -c -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0" -u FUZZ -w gau.tmp -o result_gau.tmp
                         cat result_gau.tmp | jq '[.results[]|{status: .status, length: .length, url: .url}]' | grep -oP "status\":\s(\d{3})|length\":\s(\d{1,7})|url\":\s\"(http[s]?:\/\/.*?)\"" | paste -d' ' - - - | awk '{print $2" "$4" "$6}' | sed 's/\"//g' > result_wayback.txt
                         rm *.tmp
-			echo "Done. Result is stored in result_wayback.txt"
+			printf "\nDone. Result is stored in result_wayback.txt\n"
                         break
                         ;;
                 "Beast Mode")
                         echo "Beast Mode ON"
                         mkdir ffuf
-                        xargs -P10 -I {} sh -c 'url="{}"; ffuf -c -H "X-Forwarded-For: 127.0.0.1" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0" -u "{}/FUZZ" -w wordlist/dicc.txt -t 50 -D -e js,php,bak,txt,asp,aspx,jsp,html,zip,jar,sql,json,old,gz,shtml,log,swp,yaml,yml,config,save,rsa,ppk -ac -se -o ffuf/${url##*/}-${url%%:*}.json' < alive.txt
+                        xargs -P10 -I {} sh -c 'url="{}"; ffuf -s -mc all -c -H "X-Forwarded-For: 127.0.0.1" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0" -u "{}/FUZZ" -w wordlist/dicc.txt -t 50 -D -e js,php,bak,txt,asp,aspx,jsp,html,zip,jar,sql,json,old,gz,shtml,log,swp,yaml,yml,config,save,rsa,ppk -ac -se -o ffuf/${url##*/}-${url%%:*}.json' < alive.txt
                         cat ffuf/* | jq '[.results[]|{status: .status, length: .length, url: .url}]' | grep -oP "status\":\s(\d{3})|length\":\s(\d{1,7})|url\":\s\"(http[s]?:\/\/.*?)\"" | paste -d' ' - - - | awk '{print $2" "$4" "$6}' | sed 's/\"//g' > result_beast.txt
                         rm ffuf -r
-			echo "Done. Result is stored in result_beast.txt"
+			printf "\nDone. Result is stored in result_beast.txt\n"
                         break
                         ;;
                 *)
